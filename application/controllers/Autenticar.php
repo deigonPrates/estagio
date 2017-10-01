b5924c429922871444b27f09ebe2572b5971edf1
deigon <deigon@deigon-270E5G-270E5U>
2017-10-01 19:39:30 -0300

    atualizando a funcao logar

--- application/controllers/Autenticar.php
+++ application/controllers/Autenticar.php
@@ -22,26 +22,40 @@ class Autenticar extends CI_Controller {
         if (is_null($this->session->userdata('logado'))) {
             $this->load->helper('form');
             $this->load->view('login');
-        }else{
+        } else {
             $this->load->view("inicio");
         }
-        
     }
 
     public function logar() {
 
-        $email = $this->input->post('email');
-        $senha = $this->input->post('senha');
+        $email = $this->input->post('email', TRUE);
+        $senha = $this->input->post('senha', TRUE);
 
         $this->load->model('Usuario_model');
+        $this->load->library('criptografia');
 
-        $usuarios = $this->Usuario_model->buscarUsuario($email, $senha);
+        $usuarios = $this->Usuario_model->consultar($email);
 
         if ($usuarios) {
-            $this->session->set_userdata("logado", $usuarios);
-            $this->load->view("inicio");
+
+            $bd_senha = $usuarios[0]['senha'];
+            $bd_salt = $usuarios[0]['salt'];
+
+            $descriptografia = $this->criptografia->hashHX($senha, $bd_salt);
+            if ($descriptografia['password'] === $bd_senha) {
+                $this->session->set_userdata("logado", $usuarios);
+                $this->load->view("inicio");
+            } else {
+                $erro = array('msg' => 'Email ou senha invalido!');
+                $this->load->helper('form');
+                $this->load->view('login', $erro);
+            }
         } else {
-            $this->index();
+
+            $erro = array('msg' => 'Email ou senha invalido!');
+            $this->load->helper('form');
+            $this->load->view('login', $erro);
         }
     }
 
