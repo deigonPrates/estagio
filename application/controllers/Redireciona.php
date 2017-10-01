293246a7c4ceee1ecca93ac1446fd22e4227a2db
deigon <deigon@deigon-270E5G-270E5U>
2017-09-30 23:52:36 -0300

    Criando a classe responsavel pelo redirecionamento de view para view

---
+++ application/controllers/Redireciona.php
@@ -0,0 +1,26 @@
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+/**
+ * Description of Autenticar
+ *
+ * @author deigon
+ * 
+ */
+class Redireciona extends CI_Controller {
+
+    //put your code here
+
+    public function pagina($param) {
+        
+        if (is_null($this->session->userdata('logado'))) {
+            $this->load->helper('form');
+            $this->load->view('login');
+        } else {
+            $this->load->view($param);
+        }
+    }
+
+}
