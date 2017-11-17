e7266411064e56990d8b4dbbb85846fc252662f5
deigonPrates <deigon_prates@hotmail.com>
2017-11-17 01:58:04 -0300

    modificando a base_url para a nova configuraçao do apache

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/estagio/';
+$config['base_url'] = 'http://localhost:8080/backup/estagio/';
 
 /*
 |--------------------------------------------------------------------------
